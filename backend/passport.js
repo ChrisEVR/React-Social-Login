e53aded179801da5ecac6166de056cd42b081b88
Christian <christianvargasraudales@gmail.com>
2021-11-19 02:40:39 -0600

    Primer commit

---
+++ backend/passport.js
@@ -0,0 +1,68 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GithubStrategy = require("passport-github2").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const InstagramStrategy = require("passport-instagram").Strategy;
+const dotenv = require("dotenv");
+const passport = require("passport");
+
+dotenv.config();
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: "/auth/google/callback",
+        },
+        function (accessToken, refreshToken, profile, done){
+            done(null, profile);
+        }
+    )
+);
+
+passport.use(
+    new GithubStrategy(
+        {
+            clientID: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            callbackURL: "/auth/github/callback",
+        },
+        function (accessToken, refreshToken, profile, done){
+            done(null, profile);
+        }
+    )
+);
+
+passport.use(
+    new FacebookStrategy(
+        {
+            clientID: process.env.FACEBOOK_APP_ID,
+            clientSecret: process.env.GITHUB_APP_SECRET,
+            callbackURL: "/auth/facebook/callback",
+        },
+        function (accessToken, refreshToken, profile, done){
+            done(null, profile);
+        }
+    )
+);
+
+passport.use(
+    new InstagramStrategy(
+        {
+            clientID: process.env.INSTAGRAM_APP_ID,
+            clientSecret: process.env.INSTAGRAM_APP_SECRET,
+            callbackURL: "/auth/instagram/callback",
+        },
+        function (accessToken, refreshToken, profile, done){
+            done(null, profile);
+        }
+    )
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
---
+++ client/src/data.js
@@ -0,0 +1,44 @@
+export const posts = [
+    {
+        id: 1,
+        title: "Curso de programación",
+        img: "https://images.pexels.com/photos/2004161/pexels-photo-2004161.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eu.",
+        longDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet nulla a mauris tincidunt convallis in eu risus. Nam pharetra, sapien vel rhoncus scelerisque, velit arcu finibus ante, et maximus elit purus a mauris. Praesent scelerisque dolor eu metus ultrices, nec fermentum nibh tincidunt. Phasellus tincidunt lectus elit, id suscipit dui euismod eu. Suspendisse potenti. Phasellus blandit sagittis semper. Suspendisse pellentesque quam id turpis ornare, nec placerat risus ullamcorper. Nunc condimentum at quam ac sagittis.",
+    },
+    {
+        id: 2,
+        title: "Curso de blockchain",
+        img: "https://images.pexels.com/photos/7712570/pexels-photo-7712570.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque facilisis purus.",
+        longDesc: "Curabitur placerat aliquam pulvinar. Morbi nec nibh condimentum, gravida mi fringilla, mattis nibh. Ut euismod est eget ex sollicitudin elementum. Fusce magna ipsum, convallis at commodo id, mollis sit amet lacus. Pellentesque consequat vulputate congue. Praesent porttitor nulla enim, a finibus nisi pellentesque at. Ut et ullamcorper quam. Sed viverra neque at dui lobortis cursus. Donec eget porta erat, nec sagittis turpis. Proin congue ligula eu augue luctus lacinia.",
+    },
+    {
+        id: 3,
+        title: "Curso de programación web",
+        img: "https://images.pexels.com/photos/160107/pexels-photo-160107.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque luctus tempus libero.",
+        longDesc: "Mauris ut massa tellus. Cras non diam vel turpis luctus sodales. Fusce tincidunt elit diam, sed bibendum urna lacinia sed. Curabitur et ipsum rutrum, finibus augue quis, vehicula nunc. Vivamus ut erat nisl. Cras tempus lacinia lorem, eu rutrum lectus tristique eu. Aenean gravida suscipit fermentum. Pellentesque nec tincidunt ipsum. Vivamus commodo magna neque, eget commodo turpis congue vel. Aliquam sem ex, fermentum et ultricies ut, egestas vel libero. Duis sit amet turpis vulputate, lobortis turpis quis, aliquet erat. Duis vitae sodales ligula. Donec nec elementum risus, quis laoreet velit. Donec pulvinar rutrum posuere.",
+    },
+    {
+        id: 4,
+        title: "Curso de HTML",
+        img: "https://images.pexels.com/photos/1591061/pexels-photo-1591061.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer scelerisque eu nulla ac gravida.",
+        longDesc: "Donec id tempus odio, non fringilla metus. Fusce pellentesque dui tortor, eu bibendum neque convallis sed. Morbi id pretium mauris, ac elementum magna. Donec efficitur, nisl vel ultrices rutrum, elit neque lacinia ex, in dignissim elit nibh quis lacus. Sed arcu mauris, viverra ut congue vel, mattis sit amet augue. Duis blandit malesuada sapien, id suscipit ex fermentum ac. Fusce aliquet laoreet magna eu blandit. Duis dictum dictum ipsum. Quisque consequat velit at enim feugiat, at fringilla risus iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas at ornare ligula.",
+    },
+    {
+        id: 5,
+        title: "Curso de diseño",
+        img: "https://images.pexels.com/photos/196644/pexels-photo-196644.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur laoreet elit at ante.",
+        longDesc: "Aliquam vitae libero at erat varius blandit sed nec arcu. Nullam at ligula euismod arcu dapibus efficitur vel eget nibh. Phasellus vitae lorem id nunc sodales tristique. Duis condimentum in nunc nec dictum. Fusce pulvinar imperdiet risus, eget semper mauris gravida eu. Sed sed cursus ex. Curabitur vehicula lobortis lorem, ut accumsan justo pharetra at. In hac habitasse platea dictumst. Pellentesque non mattis ex. Donec suscipit leo nulla, eget congue turpis condimentum non. Nulla sit amet facilisis odio, eu ullamcorper odio. Mauris at sem nec ex eleifend sollicitudin eget ac tellus. Morbi nec rutrum ipsum. Proin tincidunt velit eget risus consectetur iaculis. Nunc ex eros, fringilla ut nibh et, blandit malesuada mauris.",
+    },
+    {
+        id: 6,
+        title: "Curso de Inteligencia Artificial",
+        img: "https://images.pexels.com/photos/373543/pexels-photo-373543.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis elementum nibh vitae diam posuere, non.",
+        longDesc: "Curabitur venenatis imperdiet imperdiet. Fusce mattis odio a consectetur facilisis. Ut congue enim at nisi auctor, sit amet molestie nunc mattis. Vestibulum sit amet nibh vitae tellus fringilla auctor vitae at neque. Aliquam accumsan, ligula ut tristique consectetur, velit ante dictum tellus, nec hendrerit enim tortor ac sem. Pellentesque lacus dolor, porttitor eu felis sed, interdum finibus sem. Integer eget erat efficitur, tristique nisl eget, pellentesque augue. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec vehicula mi cursus ante tincidunt mattis.",
+    },
+];
